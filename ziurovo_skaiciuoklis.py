9a270d0544de98b6e6f217d368cd970cabfa3914
eid_gabriele <eid.gabriele@gmail.com>
2022-09-29 14:13:45 +0300

    klase Ziurovas, dedami filmai i ziurovo sarasa

--- ziurovo_skaiciuoklis.py
+++ ziurovo_skaiciuoklis.py
@@ -1,15 +1,31 @@
+
+
 class Filmas():
     def __init__(self, pavadinimas, metai, trukme, salis):
         self.pavadinimas = pavadinimas
         self.metai = metai
         self.trukme = trukme
         self.salis = salis
+    
+    def get_filmas(self):
+        return self.pavadinimas, self.metai, self.trukme, self.salis
+
+    def get_trukme(self):
+        return self.trukme
 
 class Serialas(Filmas):
     def __init__(self, pavadinimas, metai, trukme, salis, sezonai, serijos):
         super().__init__(pavadinimas, metai, trukme, salis)
         self.sezonai = sezonai
         self.serijos = serijos
+
+    def get_filmas(self):
+        return self.pavadinimas, self.metai, self.trukme, self.salis, self.sezonai, self.serijos
+
+    def get_trukme(self):
+        return self.trukme
+
+    
 
 
 class Kolekcija():
@@ -30,15 +46,30 @@ class Kolekcija():
             elif isinstance(irasas, Serialas):
                 print(irasas.pavadinimas, irasas.metai, irasas.trukme, irasas.salis, irasas.sezonai, irasas.serijos)
     
-    def gauti_filma_seriala(self):
-        pass
+    def gauti_filma_seriala(self, pavadinimas):
+        for irasas in self.katalogas:
+            if pavadinimas == irasas.pavadinimas:
+                return irasas.get_filmas()
+
 
 class Ziurovas():
+
     ziurovo_sarasas = []
 
     def __init__(self, vardas):
         self.vardas = vardas
 
+    def itraukti_filma(self, pavadinimas):
+        pridedamas_filmas_serialas = kolekcija.gauti_filma_seriala(pavadinimas)
+        if pridedamas_filmas_serialas != None:
+            self.ziurovo_sarasas.append(pridedamas_filmas_serialas)
+        else:
+            print("ner tokio")
+
+    
+    def spausdinti_ziurovo_sarasa(self):
+        for irasas in self.ziurovo_sarasas:
+            print(irasas)
 
 kolekcija = Kolekcija()
 
@@ -48,3 +79,7 @@ kolekcija.prideti_seriala("The Office", 2005, 22, "USA", 9, 6)
 kolekcija.prideti_seriala("Cowboy Bebop", 1998, 24, "Japan", 1, 26)
 
 kolekcija.spausdinamas_katalogas()
+
+ziurovas = Ziurovas("Jonas")
+ziurovas.itraukti_filma("Cowboy Bebop")
+ziurovas.spausdinti_ziurovo_sarasa()
